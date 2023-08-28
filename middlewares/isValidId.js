68226a6053d4736030d2dfdd43e9a62568ed5832
Oleksandr Karpiuk <113094326+Oleksandr-Karpiuk@users.noreply.github.com>
2023-08-28 14:40:33 +0300

    Update isValidId.js

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -2,7 +2,7 @@ const { isValidObjectId } = require("mongoose");
 const { HttpError } = require("../helpers");
 
 const isValidId = (req, res, next) => {
-  if (!isValidObjectId(req.params.contactId || req.params.userId)) {
+  if (!isValidObjectId(req.params.contactId) || !isValidObjectId(req.params.userId)) {
     next(
       HttpError(
         400,
